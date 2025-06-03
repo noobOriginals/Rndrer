8509361575b3ccef57b8fd2ed4cdcf9cc129daa4
noobOriginals <noobOriginals@xx.com>
2025-06-03 15:20:25 +0300

    Added extra math to vec.hpp

--- src/Header Files/util/vec.hpp
+++ src/Header Files/util/vec.hpp
@@ -3,6 +3,7 @@
 
 // Std includes
 #include <iostream>
+#include <cmath>
 
 // Local includes
 #include <util/types.h>
@@ -28,6 +29,9 @@ struct vec3 {
         e[1] = v[1];
         e[2] = v[2];
     }
+    float32 len() {
+        return std::sqrt(x * x + y * y + z * z);
+    }
     float32& operator[](const int32 i) {
         return e[i];
     }
@@ -39,45 +43,59 @@ struct vec3 {
     }
 };
 // Vec3 Operators
-vec3 operator+(vec3& v1, vec3& v2) {
-    return vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+vec3 operator+(vec3 v0, vec3 v1) {
+    return vec3(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
 }
-vec3 operator-(vec3& v1, vec3& v2) {
-    return -v2 + v1;
+vec3 operator-(vec3 v0, vec3 v1) {
+    return -v1 + v0;
 }
-vec3 operator+(vec3& v, float32 f) {
+vec3 operator+(vec3 v, float32 f) {
     return vec3(v.x + f, v.y + f, v.z + f);
 }
-vec3 operator+(float32 f, vec3& v) {
+vec3 operator+(float32 f, vec3 v) {
     return v + f;
 }
-vec3 operator-(vec3& v, float32 f) {
+vec3 operator-(vec3 v, float32 f) {
     return -f + v;
 }
-vec3 operator-(float32 f, vec3& v) {
+vec3 operator-(float32 f, vec3 v) {
     return -v + f;
 }
-vec3 operator*(vec3& v, float32 f) {
+vec3 operator*(vec3 v, float32 f) {
     return vec3(v.x * f, v.y * f, v.z * f);
 }
-vec3 operator*(float32 f, vec3& v) {
+vec3 operator*(float32 f, vec3 v) {
     return v * f;
 }
-vec3 operator/(vec3& v, float32 f) {
+vec3 operator/(vec3 v, float32 f) {
     return vec3(v.x / f, v.y / f, v.z / f);
 }
-vec3 operator/(float32 f, vec3& v) {
+vec3 operator/(float32 f, vec3 v) {
     return vec3(f / v.x, f / v.y, f / v.z);
 }
-vec3 operator*(vec3& v1, vec3& v2) {
-    return vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+vec3 operator*(vec3 v0, vec3 v1) {
+    return vec3(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
 }
-vec3 operator/(vec3& v1, vec3& v2) {
-    return v1 * (1 / v2);
+vec3 operator/(vec3 v0, vec3 v1) {
+    return v0 * (1 / v1);
 }
-std::ostream& operator<<(std::ostream& os, vec3& v) {
+std::ostream& operator<<(std::ostream& os, vec3 v) {
     os << v.x << "\n" << v.y << "\n" << v.z;
     return os;
+}
+// Other vec3 operators
+float32 dot3(vec3 v0, vec3 v1) {
+    return v0.x * v1.x + v0.y + v1.y + v0.z * v1.z;
+}
+vec3 cross3(vec3 v0, vec3 v1) {
+    vec3 vec;
+    vec.x = v0.y * v1.z - v0.z * v1.y;
+    vec.y = v0.z * v1.x - v0.x * v1.z;
+    vec.z = v0.x * v1.y - v0.y * v1.x;
+    return vec;
+}
+vec3 normalize3(vec3 v) {
+    return v / v.len();
 }
 
 #endif
